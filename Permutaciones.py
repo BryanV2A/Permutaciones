57b781f385fa4e4d8c1523f4cc176d7627636788
Bryan Alonso Valencia <AlonsoVal2.03@gmail.com>
2019-09-26 20:45:22 -0500

    feat: programa de forma recursiva de permutaciones

--- Permutaciones.py
+++ Permutaciones.py
@@ -1,1 +1,20 @@
 class Permutaciones:
+
+    # Fórmula: Pn = n!
+
+    def permutacion(self, n):
+
+        if (n == 0):
+            return 1
+
+        else:
+            return self.permutacion(n - 1) * n
+
+def main():
+    
+    ejecutar = Permutaciones()
+    r = ejecutar.permutacion(6)
+    print(r)
+
+if __name__ == '__main__':
+    main()
